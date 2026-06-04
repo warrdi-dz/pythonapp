b8fa2df18861cbd0eebc062e750873163a6369b0
warrdi-dz <myjob1613@gmail.com>
2026-06-04 09:29:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -284,6 +284,13 @@ def analyse():
         # MASQUE CARROSSERIE
         # ===============================================
         hsv_full  = cv2.cvtColor(car_crop, cv2.COLOR_BGR2HSV)
+        h, s, v = cv2.split(hsv_full)
+        # Pixels très sombres = ombres
+        mask_shadow = cv2.inRange(v, 0, 70)
+        # Pixels très lumineux = reflets
+        mask_reflect = cv2.inRange(v, 220, 255)
+        mask_body = cv2.bitwise_and(mask_body,cv2.bitwise_not(mask_shadow))
+        mask_body = cv2.bitwise_and(mask_body,cv2.bitwise_not(mask_reflect))
         mask_dark = cv2.inRange(hsv_full, (0, 0, 0),   (180, 255, 45))
         mask_sky  = cv2.inRange(hsv_full, (0, 0, 210), (180, 18, 255))
         mask_body = cv2.bitwise_not(cv2.bitwise_or(mask_dark, mask_sky))
