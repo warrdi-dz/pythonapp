6c5929a88640a8a1ecf575cf398ed6f2302e7535
warrdi-dz <myjob1613@gmail.com>
2026-06-07 00:07:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -568,7 +568,7 @@ def analyse():
                 color_rect, label_score, diff, verdict = (150,150,150), "N/A", 0.0, "Non analysable"
             else:
                 diff = float(np.linalg.norm(zone_color - ref_color))
-                if 12.2 <= diff < 26:
+                if 12.4 <= diff < 26:
                     color_rect, verdict = (0, 0, 255),   "Peinture refaite!";  detected += 1
                 elif diff < 14:
                     color_rect, verdict = (0, 165, 255), "Variation suspecte"; detected += 1
