9f89b766cc328326d80fe60254301669792e133e
warrdi-dz <myjob1613@gmail.com>
2026-06-07 15:10:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -73,26 +73,18 @@ def home():
 # =========================
 def get_poly_color(hsv_img, body_mask, polygon):
     h, w = hsv_img.shape[:2]
-
     poly_mask = np.zeros((h, w), dtype=np.uint8)
     cv2.fillPoly(poly_mask, [np.array(polygon, dtype=np.int32)], 255)
-
     combined = cv2.bitwise_and(poly_mask, body_mask)
     valid = hsv_img[combined > 0]
-
     if len(valid) < 80:
-        return None, 0, 0, 0
+        return None, 0
+    return np.array([
+        float(np.median(valid[:, 0])),
+        float(np.median(valid[:, 1])),
+        float(np.median(valid[:, 2]))
+    ]), len(valid)
 
-    color = np.array([
-        float(np.median(valid[:,0])),
-        float(np.median(valid[:,1])),
-        float(np.median(valid[:,2]))
-    ])
-
-    std_s = float(np.std(valid[:,1]))
-    std_v = float(np.std(valid[:,2]))
-
-    return color, len(valid), std_s, std_v
 
 # =========================
 # REFINE CROP
@@ -570,23 +562,20 @@ def analyse():
                 [[x1 + p[0], y1 + p[1]] for p in poly_local], dtype=np.int32
             )
 
-            zone_color, px_count,std_s, std_v = get_poly_color(hsv_full, mask_body, poly_local)
+            zone_color, px_count = get_poly_color(hsv_full, mask_body, poly_local)
 
             if zone_color is None:
                 color_rect, label_score, diff, verdict = (150,150,150), "N/A", 0.0, "Non analysable"
             else:
                 diff = float(np.linalg.norm(zone_color - ref_color))
-                if 10 >= diff < 14:
-                    color_rect, verdict = (0,0,255), "Peinture refaite!"
-                    detected += 1
-                elif diff <= 10 and (std_s > 22 or std_v > 25):
-                    color_rect, verdict = (0,0,255), "Peinture refaite!"
-                    detected += 1
-                elif diff <= 6:
-                    color_rect, verdict = (0,165,255), "Variation suspecte"
-                    detected += 1
+                if 14 <= diff < 26:
+                    color_rect, verdict = (0, 0, 255),   "Peinture refaite!";  detected += 1
+                elif 12.2<= diff <12.3:
+                    color_rect, verdict = (0, 0, 255), "Peinture refaite!"; detected += 1    
+                elif diff < 14:
+                    color_rect, verdict = (0, 165, 255), "Variation suspecte"; detected += 1
                 else:
-                     color_rect, verdict = (0,210,0), "OK"
+                    color_rect, verdict = (0, 210, 0),   "OK"
                 label_score = str(int(diff))
 
             overlay = final_img.copy()
