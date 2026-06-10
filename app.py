85c4a2fba9e46ec2ebbc3e4fc9b53d8a8820d6a4
warrdi-dz <myjob1613@gmail.com>
2026-06-10 14:23:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -590,7 +590,7 @@ def analyse():
                 suspect_satur   = std_s > 10 
                 suspect_texture = std_v < 30
 
-                if suspect_satur  :
+                if suspect_texture :
                     color_rect, verdict = (0, 0, 255),   "Peinture refaite!";  detected += 1
                 elif suspect_color or (suspect_satur and suspect_texture):
                     color_rect, verdict = (0, 165, 255), "Variation suspecte"; detected += 1
