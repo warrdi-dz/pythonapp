6450c8db2ce01c8fe8e5be4a7a748b2ed2ae5db1
warrdi-dz <myjob1613@gmail.com>
2026-06-10 11:41:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -587,7 +587,7 @@ def analyse():
                 #   - std_s             : empreinte chimique de la peinture
                 #   - std_v             : texture / mastic / grain
                 suspect_color   = 7>= diff  >= 4
-                suspect_satur   = std_s < 7
+                suspect_satur   = std_s < 5
                 suspect_texture = std_v < 30
 
                 if suspect_color and suspect_satur:
